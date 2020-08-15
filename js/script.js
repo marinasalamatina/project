79560f00df02ba84d782a2ae0aaa519f51294811
Marina <marinasalamatina@mail.ru>
2020-08-15 20:45:51 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const isChecked = 1,
+      isClose = 1;
+
+console.log(2+2*2 !== '6');
---
+++ jsReact_курс.js
@@ -0,0 +1,19 @@
+const start = document.querySelector('#start'),
+progress = document.querySelector('#progress'),
+previews = document.querySelectorAll('.preview');
+
+start.addEventListener('click', work);
+
+function work() {
+    let counter = 0;
+
+    const id = setInterval(() => {
+        progress.value += 100/previews.length;
+        previews[counter].classList.toggle('ready');
+        counter++;
+
+        if (counter === previews.length) {
+            clearInterval(id);
+        }
+    }, 500);
+}
