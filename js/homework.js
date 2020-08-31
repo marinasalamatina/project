996769b1d6594206c4e225af00831ab745c84c6b
Marina <marinasalamatina@mail.ru>
2020-08-31 13:21:31 +0300

    Prototype-oriented inheritance

--- js/homework.js
+++ js/homework.js
@@ -70,3 +70,34 @@
 //}
 //
 //detectPersonalLevel();
+
+//                        Основы ООП, ПРОТОТИПНО-ОРИЕНТИРОВАННОЕ НАСЛЕДОВАНИЕ               
+
+//let str = "some";
+//let strObj = new String(str);
+//
+//console.log(typeof(str));
+//console.log(typeof(strObj));
+
+//console.dir([1,2,3]);
+
+//const soldier = {
+//    health: 400,
+//    armor: 100,
+//    sayHello: function()
+//    {
+//        console.log('Hello!')
+//    }
+//};
+//
+//    const john = Object.create(soldier);
+////    устаревший формат
+//
+////john.__proto__ = soldier;
+//
+////    свежий формат
+//
+//Object.setPrototypeOf(john, soldier);
+//
+//john.sayHello();
+//console.log(john.health);
